f9835a37a4527a81e8dfaa54a079073583a70720
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-05-26 14:35:40 +0700

    feat: bersihkan root dari file lama + tambah .env.example per app

--- resources/views/components/button.blade.php
+++
@@ -1,8 +0,0 @@
-<button type="{{ $type ?? 'button' }}" 
-        class="btn btn-{{ $color ?? 'primary' }} {{ $class ?? '' }}"
-        {{ $attributes }}>
-    @if(isset($icon))
-    <i class="{{ $icon }} {{ isset($slot) && !empty($slot) ? 'me-2' : '' }}"></i>
-    @endif
-    {{ $slot }}
-</button> 
